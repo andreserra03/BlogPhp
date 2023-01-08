5144d3572aa98f8e5e47d02f3905b6f2dc09c4e5
André Serra <andreserra22@gmail.com>
2023-01-08 19:21:05 +0000

    delete deleted

--- src/interfaces/user/perfil.php
+++ src/interfaces/user/perfil.php
@@ -13,12 +13,6 @@ include('../shared/sidebar.php');
 	<hr>
 	<hp>Utilizador: <?php echo $_GET['u']; ?></hp>
 	<hr>
-	<div class="mt-5">
-		<form action="delete.php" method="post">
-			<input hidden type="text" name="id" value="<?php echo $_SESSION['id_user']; ?>">
-			<button name="btn_delete" type="submit" class="btn btn-danger">Delete Account</button>
-		</form>
-	</div>
 </div>
 
 <?php
